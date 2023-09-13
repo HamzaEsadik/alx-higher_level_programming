a087e2ebf6faed99510e6d6dcc4f78890f5e0d7d
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-09-13 19:58:13 +0100

    task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at idx position
+ * @head: pointer to the list
+ * @idx: index
+ * @n: data
+ * Return: adress of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, int idx, int n)
+{
+	listint_t *new;
+	listint_t *ptr = *head;
+	int i;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+	new->next = *head;
+	*head = new;
+	return (new);
+	}
+	for (i = 0; ptr; ++i)
+	{
+	if (i == idx - 1)
+	{
+		new->next = ptr->next;
+		ptr->next = new;
+		return (new);
+	}
+	ptr = ptr->next;
+	}
+	free(new);
+	return (NULL);
+}
+
+/**
+ * insert_node - inster note at the correct idx
+ * @head: linked list
+ * @number: value at the node
+ * Return: pointer to new node or NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	if (head != NULL)
+	{
+		listint_t *tmp;
+		listint_t *new;
+		int idx;
+
+		idx = 0;
+		tmp = *head;
+		while (number > tmp->n)
+		{
+			if (tmp->next == NULL)
+			{
+				idx++;
+				break;
+			}
+			else
+			{
+				tmp = tmp->next;
+				idx++;
+			}
+		}
+		new = insert_nodeint_at_index(head, idx, number);
+		return (new);
+	}
+	else
+		return (NULL);
+}

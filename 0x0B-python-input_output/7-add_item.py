2449fad93f89b82d21c5aee87ab0fef5d1428ab7
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-10-11 00:44:39 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-Module for adds all arguments to a Python list,
-and then save them to a file
+Module for adds all arguments to a Python list.
 """
 import json
-import sys
 import os.path
+import sys
 save_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_jason_file = __import__('6-load_from_json_file').load_from_json_file
 

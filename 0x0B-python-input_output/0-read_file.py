bbcee0d3af1d06709257f1b165ce902226df2710
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-10-15 16:49:25 +0100

    task 11

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,6 @@ def read_file(filename=""):
     """
     Method that reads and prints a text file (UTF8)
     """
-    with open(filename, encoding="utf-8") as my_file_0:
-        print(my_file_0.read(), end="")
+    with open(filename) as my_file_0:
+        content = my_file_0.read()
+        print(content)

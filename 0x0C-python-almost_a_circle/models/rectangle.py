cf2fd8adff48d47022bbc1a7b0bc3f4db3c9f91d
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-10-17 13:07:48 +0100

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,11 +5,12 @@ Base = __import__("base").Base
 
 class Rectangle(Base):
     '''Rectangle Class that enherit from Base'''
+    
     def __init__(self, width, height, x=0, y=0, id=None):
         '''
         Init Method for Rectangle Class
 
-        Args:
+        Arguments:
             -width: width of Rectangle
             -width: width of Rectangle
             -x: int arg
@@ -20,7 +21,6 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
 
     @property
     def width(self):

53c951786f6816fd282d9fd9a5259a7c7f64f827
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-10-17 18:52:37 +0100

    task 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,3 +32,32 @@ class Base:
             else:
                 list_dictionaries = [obj.to_dictionary() for obj in list_objs]
                 json_file.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                y_instance = cls(3, 7)
+            else:
+                y_instance = cls(3)
+            y_instance.update(**dictionary)
+            return y_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances."""
+        file_name = str(cls.__name__) + ".json"
+        try:
+            with open(file_name, "r") as json_file:
+                list_dictionaries = Base.from_json_string(json_file.read())
+                return [cls.create(**dicti) for dicti in list_dictionaries]
+        except IOError:
+            return []

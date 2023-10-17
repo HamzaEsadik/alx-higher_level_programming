678f83a91f98ab98a676c22f4cc6b61d51bcb701
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-10-17 18:18:03 +0100

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,13 +22,18 @@ class Square(Rectangle):
             - y: int
         """
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """
         Return a string representation of the rectangle.
-
-        Format:
-            "[Square] (<id>) <x>/<y> - <size>"
         """
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "\
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - "\
                f"{self.size}"
+
+if __name__ == "__main__":
+
+    s1 = Square(5)
+    print(s1)
+    print(s1.area())
+    s1.display()

65dff43ed2459861b6df8ae1ad617e16dd6431ba
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-10-17 17:50:51 +0100

    task 1

--- 0x0C-python-almost_a_circle/models/save.py
+++ 0x0C-python-almost_a_circle/models/save.py
@@ -109,3 +109,58 @@ class Rectangle(Base):
             raise ValueError(f"y must be >= 0")
         else:
             self.__y = y
+
+    def area(self):
+        """returns area of rectangle"""
+        return self.__height * self.__width
+
+    def display(self):
+        """print Rectangel as # to stdout"""
+        print("\n"*self.__y, end='')
+        for i in range(self.__height):
+            print(" "*self.__x, end='')
+            for j in range(self.__width):
+                print("#", end='')
+            print("\n", end='')
+
+    def __str__(self):
+        """
+        Return a string representation of the rectangle.
+
+        Format:
+            "[Rectangle] (id) x/y - width/height"
+        """
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "\
+               f"{self.__width}/{self.__height}"
+
+    def update(self, *args, **kwargs):
+        """
+        Update the attributes of the rectangle.
+
+        Args:
+            *args: Variable positional arguments.
+            **kwargs: Variable keyword arguments.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == 'width':
+                    self.width = value
+                elif key == 'height':
+                    self.height = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
+                elif key == 'id':
+                    self.id = value

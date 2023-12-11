e6966150ae0501e7dafea585e95d9057de22237e
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-12-11 19:40:42 +0100

    fix task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,5 +5,6 @@ function add (a, b) {
 
 const firstNum = parseInt(process.argv[2]);
 const secNum = parseInt(process.argv[3]);
+const res = add(firstNum, secNum);
 
-console.log(add(firstNum, secNum));
+console.log(res);

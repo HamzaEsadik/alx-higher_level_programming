6b0baa5af1cd93cae759080bf47748254dca4395
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-12-11 19:04:45 +0100

    task 8: script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,9 +4,6 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      console.log('X', '');
-    }
-    console.log('');
+    console.log('X'.repeat(size));
   }
 }

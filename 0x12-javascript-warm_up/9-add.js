cb49d5cf949c519672a289edc19c07a79d8dce8a
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-12-11 19:18:39 +0100

    task 9: prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 function add (a, b) {
-  console.log(a * b);
+  return (a * b);
 }
 
 const firstNum = parseInt(process.argv[2]);
 const secNum = parseInt(process.argv[3]);
-add(firstNum, secNum);
+
+console.log(add(firstNum, secNum));

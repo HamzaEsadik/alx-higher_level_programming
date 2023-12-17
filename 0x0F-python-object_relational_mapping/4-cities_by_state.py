f2755b420aeda419bbe21fd949d4378ea5e8d20c
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-12-17 18:40:54 +0100

    task 4: list rows of a db

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''first module'''
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+        )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()

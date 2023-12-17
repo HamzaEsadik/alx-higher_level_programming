1f64e27aab29dd45daaf90cc49e4c6f654ef2c32
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-12-17 18:00:58 +0100

    task 2: filter by arg

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''first module'''
+import MySQLdb
+import sys
+
+
+def main():
+    '''this is the main function'''
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+        )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE "
+                "name = '{}' ORDER BY id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()

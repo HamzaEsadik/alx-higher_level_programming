6b48a81f2f2340aa1c04391c81aeba99f468c967
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-12-17 21:36:02 +0100

    task 6: class states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,15 +2,18 @@
 '''model state mudule'''
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
-connection_string = 'mysql://root:usopp@localhost:3306/hbtn_0e_6_usa'
-engine = create_engine(connection_string, echo=True)
-engine.connect()
 
 class State(Base):
+    '''
+    State class.
+
+    attribites:
+        -id: the id
+        -name: state name
+    '''
     __tablename__ = "states"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False)

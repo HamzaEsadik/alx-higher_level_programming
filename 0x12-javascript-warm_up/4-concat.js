9861217910a0e2deefc26626a76bf9c0fdde3cde
Hamza Esadik <132409623+HamzaEsadik@users.noreply.github.com>
2024-01-08 22:23:03 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-const firstWord = process.argv[2];
-const secondWord = process.argv[3];
-console.log(firstWord + ' is ' + secondWord);
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const myNum = parseInt(process.argv[2], 10);
-if (isNaN(myNum)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${myNum}`);
-}

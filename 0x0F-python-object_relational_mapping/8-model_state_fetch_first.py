480c39f16f9f8ce7f1adcc68b21199922507196b
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-02-19 16:19:23 +0100

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     session = Session()
     state = session.query(State).order_by(State.id).first()
     if (state):
-      print("{}: {}".format(state.id, state.name))
+      print("{}: {}\n".format(state.id, state.name))
     else:
-       print("Nothing")
+       print("Nothing\n")
     session.close()

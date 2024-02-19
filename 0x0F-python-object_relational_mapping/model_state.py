4c72d1641db3be47e2a630dbb702a527181c2046
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-02-19 14:05:20 +0100

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,7 @@ import sys
 engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                        .format(sys.argv[1], sys.argv[2],
                                sys.argv[3]), pool_pre_ping=True)
+engine.connect()
 Base = declarative_base()
 
 

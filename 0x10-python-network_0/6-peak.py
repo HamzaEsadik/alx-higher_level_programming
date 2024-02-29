ccb21c3b161b038eb7f5fa5692124f4a324244d8
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-02-29 19:46:35 +0100

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'python interview module'
+
+def find_peak(list_of_integers):
+    'finds a peak in a list of unsorted integers'
+    if list_of_integers:
+        peak = list_of_integers[0]
+        for element in list_of_integers:
+            if element > peak:
+                peak = element
+        return peak
+    else:
+        return None

dcc6ea84628698a85128d7d73a765c30bac78b2e
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-01 20:04:54 +0100

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''task 0'''
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+    response = r.read()
+    print (f'''Body response:
+           \n\t- type: {type(response)}
+           \n\t- content: {response}
+           \n\t- utf8 content: {response.decode('utf-8')}''')

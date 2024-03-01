ed1bbe56b18779639368352fc778bac02967caf9
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-01 20:24:45 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''task 0'''
+'''task 0: fetch data'''
 
 if __name__ == "__main__":
     import urllib.request
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         print(f'''Body response:
             \n\t- type: {response.__class__}
             \n\t- content: {response}
-            \n\t- utf8 content: {response.decode('ascii')}''')
+            \n\t- utf8 content: {response.decode('utf-8')}''')

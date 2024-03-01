12b27127354efa02fc1c1c1ddd4785b7f2b0d4f1
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-01 21:36:16 +0100

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''task 0: fetch data'''
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers['X-Request-Id'])

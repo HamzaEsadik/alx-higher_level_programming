9b216bb4a5fc8b992a14a631b666af84ed5a84e9
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-01 21:58:58 +0100

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''task 0: fetch data'''
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(response.status_code)
+    else:
+        print(response.text)

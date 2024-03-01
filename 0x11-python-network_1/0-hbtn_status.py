72fde3f53a29e5e106d267ecdc2e7edf6eefce76
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-01 20:23:54 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 '''task 0'''
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import urllib.request
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        r = response.read()
-
-    print("Body response:")
-    print("\t- type: {}".format(r.__class__))
-    print("\t- content: {}".format(r))
-    print("\t- utf8 content: {}".format(r.decode('ascii')))
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as r:
+        response = r.read()
+        print(f'''Body response:
+            \n\t- type: {response.__class__}
+            \n\t- content: {response}
+            \n\t- utf8 content: {response.decode('ascii')}''')

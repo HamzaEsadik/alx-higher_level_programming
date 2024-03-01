df4deb870ede9792cec847841ccb054f228d4230
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-01 21:27:22 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import requests
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
-    print('Body response: ')
+    print('Body response:')
     print(f'\t- type: {type(response.text)}')
     print(f'\t- content: {response.text}')

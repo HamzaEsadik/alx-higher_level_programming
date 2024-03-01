0aa1242055328a6b29785328bbd187ef654a68c6
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-01 21:24:46 +0100

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''task 0: fetch data'''
+
+if __name__ == "__main__":
+    import requests
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print('Body response: ')
+    print(f'\t- type: {type(response.text)}')
+    print(f'\t- content: {response.text}')

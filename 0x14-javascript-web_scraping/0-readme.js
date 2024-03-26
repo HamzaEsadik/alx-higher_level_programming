abe33b13a1cd5c4491075571d2e2dd9b018a1018
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-26 12:32:22 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/snap/bin/node
+const FS = require('fs');
+const FILE_PATH = process.argv[2];
+
+FS.readFile(FILE_PATH, 'utf8', function (err, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});

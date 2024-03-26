fc57907888520f230cdfdd7501d89b4353229389
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-26 14:50:29 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
-const URL = process.argv[2];
 const REQUEST = require('request');
-let count = 0;
-
-REQUEST(URL, function (error, _, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    for (let i = 0; data.results[i] !== undefined; i++) {
-      if (data.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        count++;
-      }
-    }
-    console.log(count);
+const URL = process.argv[2];
+REQUEST.get(URL, function (err, _, body) {
+  let count = 0;
+  if (err) {
+    console.log(err);
   }
+  const data = JSON.parse(body);
+  for (let i = 0; data.results[i] !== undefined; i++) {
+    if (data.results[i].characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  }
+  console.log(count);
 });

301de7f6a44d33a2bfe04fe70148353c23bf1464
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-26 13:06:19 +0000

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/snap/bin/node
+const URL = process.argv[1];
+const REQUEST = require('request');
+
+REQUEST.get(URL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response.statusCode);
+  }
+});

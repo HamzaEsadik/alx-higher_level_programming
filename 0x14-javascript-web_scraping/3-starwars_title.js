5c09c7967d4ba966d455531de27f5b7b68016678
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-26 13:41:34 +0000

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const MOVIE_ID = process.argv[2];
 const REQUEST = require('request');
-const URL = 'https://swapi-api.alx-tools.com/api/films/' + MOVIE_ID
+const URL = 'https://swapi-api.alx-tools.com/api/films/' + MOVIE_ID;
 
 REQUEST.get(URL, function (error, _, body) {
   if (error) {

2084bb0400e79ecfdd9b4d556d18f8d0e6ac7f7f
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-26 13:38:16 +0000

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const MOVIE_ID = process.argv[2];
+const REQUEST = require('request');
+const URL = 'https://swapi-api.alx-tools.com/api/films/' + MOVIE_ID
+
+REQUEST.get(URL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    console.log(content.title);
+  }
+});

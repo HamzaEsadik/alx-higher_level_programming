5d4220a81a3b5ba1897c185b281d3eb801e635fa
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-26 14:02:03 +0000

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const URL = process.argv[2];
+const REQUEST = require('request');
+let count = 0;
+
+REQUEST(URL, function (error, _, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    for (let i = 0; data.results[i] !== undefined; i++) {
+      if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
